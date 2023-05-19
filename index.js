bac09edd712a045f15fd074e49fad9bc6f8a1cb7
Anjali Saun <Anjaliannusaun@gmail.com>
2023-05-19 18:00:01 +0530

    commitfirst

---
+++ index.js
@@ -0,0 +1,34 @@
+function add(){
+    let a=prompt("enter a no.");
+    let b=prompt("enter a no.");
+    a=parseInt(a);
+    b=parseInt(b);
+    document.write(a+b,"<br>");
+}
+function subtraction(){
+    let a=prompt("enter a no.");
+    let b=prompt("enter a no.");
+    a=parseInt(a);
+    b=parseInt(b);
+    document.write(a-b,"<br>");
+}
+
+function multiplication(){
+    let a=prompt("enter a no.");
+    let b=prompt("enter a no.");
+    a=parseInt(a);
+    b=parseInt(b);
+    document.write(a*b,"<br>");
+}
+
+function division(){
+    let a=prompt("enter a no.");
+    let b=prompt("enter a no.");
+    a=parseInt(a);
+    b=parseInt(b);
+    document.write(a/b,"<br>");
+}
+
+
+
+
